8f116a1a4426a130964641d0aa46adfa2abb721e
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2025-10-27 04:15:35 +0900

    feat(application): 자동차 이름 입력 및 유효성 검사 - 사용자로부터 자동차 입력 - 자동차 이름 5자 초과 시 예외 발생 - 입력값을 쉼표(,) 기준으로 나누어 리스트로 변환

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
+    private static void checkCarName(String carName) throws Exception{
+        if(carName.length()>5){
+            throw new IllegalArgumentException();
+        }
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInputCars = Console.readLine();
+        String[] userInputSplit = userInputCars.split(",");
+        List<String> carNames = Arrays.asList(userInputSplit);
+        carNames.forEach(c -> {
+            try {
+                checkCarName(c);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
+            }
+        });
+        ArrayList<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames){
+            Car car = new Car(carName);
+            cars.add(car);
+        }
     }
 }
