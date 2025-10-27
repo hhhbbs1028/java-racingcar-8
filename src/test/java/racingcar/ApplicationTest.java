4f4df9ab6f0231501e3b22c21b0cf1d7faf3f123
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2025-10-27 21:51:45 +0900

    test(application): 잘못된 입력값에 대한 예외 추가
    - 자동차 이름에 공백이 포함된 경우 예외 발생
    - 시도 횟수에 숫자가 아닌 값이 입력된 경우 예외 발생

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,22 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 이름_공백_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi, ", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 횟수_예외_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,woni", "test"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
